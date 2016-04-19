ce588e92345dacd0a42358adac7193ef585bae90
Aleksandr Mishchaniuk <map.m@ua.fm>
2016-04-19 22:45:52 +0300

    all methods from http://ruby-doc.org/core-2.0.0/Struct.html

--- factory.rb
+++ factory.rb
@@ -23,9 +23,36 @@ class Factory
         vals = instance_variables.map { |var| instance_variable_get(var) }
         vals * ', '
       end
+      alias_method :inspect, :to_s
 
       define_method :== do |other|
-        self.to_s === other.to_s
+        return true if self.object_id == other.object_id
+        self.instance_variables.inject(true) do |f, var|
+          return false unless f
+          begin
+            s = self.instance_variable_get(var)
+            o = other.instance_variable_get(var)
+            f && (s == o)
+          rescue Exception => e
+            puts e.message
+            false
+          end
+        end
+      end
+
+      define_method :eql? do |other|
+        return true if self.object_id == other.object_id
+        self.instance_variables.inject(true) do |f, var|
+          return false unless f
+          begin
+            s = self.instance_variable_get(var)
+            o = other.instance_variable_get(var)
+            f && (s.eql? o)
+          rescue  Exception => e
+            puts e.message
+            false
+          end
+        end
       end
 
       define_method :[] do |i|
@@ -55,6 +82,55 @@ class Factory
           var1 = var.to_s.delete "@"
           block.call(var1, instance_variable_get(var)) if block
         end
+      end
+
+      define_method :hash do
+        instance_variables.inject(17) do |code, var|
+          37*code + instance_variable_get(var).hash
+        end
+      end
+
+      define_method :length do; instance_variables.size; end
+      alias_method :size, :length
+
+      define_method :members do
+       instance_variables.map { |var| var.to_s.delete('@').to_sym }
+      end
+
+      define_method :select do |&block|
+        instance_variables.inject([]) do |arr, var|
+          val = instance_variable_get(var)
+          arr.push(val) if block.call(val)
+          arr
+        end
+      end
+
+      define_method :to_a do
+        instance_variables.map do |var|
+          instance_variable_get(var)
+        end
+      end
+      alias_method :values, :to_a
+
+      define_method :to_h do
+        instance_variables.inject(Hash.new) do |hash, var|
+          hash[var.to_s.delete('@').to_sym] = instance_variable_get(var)
+          hash
+        end
+      end
+
+      define_method :values_at do |*args|
+        nums = args.inject([]) do |arr, arg|
+          if arg.is_a? Integer
+            arr << arg
+          elsif arg.is_a? Range
+            arg.each {|a| arr << a }
+          else
+            raise 'One of parameters is not Integer or Range'
+          end 
+          arr           
+        end
+        nums.map { |num| self[num-1] }
       end
 
     end
--- test.rb
+++ test.rb
@@ -18,24 +18,64 @@ puts c.address === "123 Main"
 joe   = Customer.new("Joe Smith", "123 Maple, Anytown NC", 12345)
 joejr = Customer.new("Joe Smith", "123 Maple, Anytown NC", 12345)
 jane  = Customer.new("Jane Doe", "456 Elm, Anytown NC", 12345)
+puts '_____________==__________________'
 puts joe == joejr   #=> true
 puts !(joe == jane)    #=> false
 
+jeck = joe
+puts '___________eql?_____________________'
+puts joe.eql? jeck
+puts joe.eql? joejr
+
+puts '____________[]_____________________'
 puts joe["name"] == "Joe Smith"  #=> "Joe Smith"
 puts joe[:name] == "Joe Smith"  #=> "Joe Smith"
 puts joe[0] == "Joe Smith"      #=> "Joe Smith"
 
+puts '_____________[]=_____________________'
 joe["name"] = "Luke"
 joe[:zip]   = 90210
 puts joe.name == "Luke" #=> "Luke"
 puts joe.zip == 90210  #=> "90210"
 
+puts '______________each___________________'
 arr = []
 joe.each {|x| arr.push(x) }
 puts arr === ["Luke", "123 Maple, Anytown NC", 90210]
 
+puts '_______________each_pair________________'
 arr = []
 joe.each_pair {|name, value| arr.push("#{name} => #{value}") }
 puts arr[0] == "name => Luke"
 puts arr[1] == "address => 123 Maple, Anytown NC"
 puts arr[2] == "zip => 90210"
+
+puts '_____________hash_______________________'
+puts joe.hash
+
+puts '_____________to_s_______________________'
+puts joe.to_s
+
+puts '_____________length_______________________'
+puts joe.length == 3
+
+puts '_____________members_______________________'
+puts joe.members === [:name, :address, :zip]
+
+puts '_____________select_______________________'
+Lots = Factory.new(:a, :b, :c, :d, :e, :f)
+l = Lots.new(11, 22, 33, 44, 55, 66)
+puts l.select {|v| (v % 2).zero? } == [22, 44, 66] #=> [22, 44, 66]
+
+puts '_____________size_______________________'
+puts joe.size == 3
+
+puts '_____________to_a_______________________'
+puts joe.to_a == ["Luke", "123 Maple, Anytown NC", 90210]
+
+puts '_____________to_h_______________________'
+puts joe.to_h == {name: "Luke", address: "123 Maple, Anytown NC", zip: 90210}
+
+puts '_____________values_at_______________________'
+puts joe.values_at(1...4) == ["Luke", "123 Maple, Anytown NC", 90210]
+puts joe.values_at(2, 3) == ["123 Maple, Anytown NC", 90210]
